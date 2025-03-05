4fb53c85e772c17a35982c9078f0291817418853
sdsmnc221 <antr.2201@gmail.com>
2025-03-05 13:49:21 +0100

    fix(app): up

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.DEV
+  baseURL: import.meta.env.DEV
     ? import.meta.env.VITE_BASE_URL
     : "/api" + import.meta.env.VITE_BASE_URL_VERCEL,
   headers: {
