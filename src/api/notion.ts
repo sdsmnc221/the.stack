214dfbd11db0cec2c66240afedc0c31adb717a6f
sdsmnc221 <antr.2201@gmail.com>
2025-03-05 14:04:36 +0100

    fix(app): up

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -30,6 +30,11 @@ export default async function handler(
         "Content-Type": "application/json",
         Authorization: atob(request.headers.authorization),
         "Notion-Version": "2022-06-28",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
       },
       body: JSON.stringify(request.body),
     });
